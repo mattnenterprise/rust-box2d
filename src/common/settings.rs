282212f4dfa39a8c5ac4e80ab6e2b08c2a4969b7
Matt McCoy <mattnenterprise@yahoo.com>
2016-07-15 17:53:17 -0400

    Adding more settings from box2d

--- src/common/settings.rs
+++ src/common/settings.rs
@@ -1,6 +1,6 @@
 use std::f32::consts::PI;
 
-// Collision
+/// Collision
 
 /// The maximum number of contact points between two convex shapes. Do
 /// not change this value.
@@ -39,3 +39,38 @@ const MAX_SUB_STEPS: i32 = 8;
 
 /// Maximum number of contacts to be handled to solve a TOI impact.
 const MAX_TOI_CONTACTS: i32 = 32;
+
+/// A velocity threshold for elastic collisions. Any collisions with a relative linear
+/// velocity below this threshold will be treated as inelastic.
+const VELOCITY_THRESHOLD: f32 = 1.0;
+
+/// The maximum linear position correction used when solving constraints. This helps to
+/// prevent overshoot.
+const MAX_LINEAR_CORRECTION: f32 = 0.2;
+
+/// The maximum linear velocity of a body. This limit is very large and is used
+/// to prevent numerical problems. You shouldn't need to adjust this.
+const MAX_TRANSLATION: f32 = 2.0;
+const MAX_TRANSLATION_SQUARED: f32 = MAX_TRANSLATION * MAX_TRANSLATION;
+
+/// The maximum linear velocity of a body. This limit is very large and is used
+/// to prevent numerical problems. You shouldn't need to adjust this.
+const MAX_ROTATION: f32 = 0.5 * PI;
+const MAX_ROTATION_SQUARED: f32 = MAX_ROTATION * MAX_ROTATION;
+
+/// This scale factor controls how fast overlap is resolved. Ideally this would be 1 so
+/// that overlap is removed in one time step. However using values close to 1 often lead
+/// to overshoot.
+const BAUMGARTE: f32 = 0.2;
+const TOI_BAUGARTE: f32 = 0.75;
+
+/// Sleep
+
+/// The time that a body must be still before it will go to sleep.
+const TIME_TO_SLEEP: f32 = 0.5;
+
+/// A body cannot sleep if its linear velocity is above this tolerance.
+const LINEAR_SLEEP_TOLERANCE: f32 = 0.01;
+
+/// A body cannot sleep if its angular velocity is above this tolerance.
+const ANGULAR_SLEEP_TOLERANCE: f32 = (2.0 / 180.0 * PI);
