e8da9ddd44691cdbc452330fb4313a1fa53f9fdb
Matt McCoy <mattnenterprise@yahoo.com>
2016-07-15 13:57:03 -0400

    Fixing examples to use new SFML

--- examples/bouncing_circle.rs
+++ examples/bouncing_circle.rs
@@ -3,8 +3,8 @@ extern crate box2d;
 extern crate time;
 
 use sfml::system::Vector2f;
-use sfml::window::{ContextSettings, VideoMode, event, Close};
-use sfml::graphics::{RenderWindow, RenderTarget, CircleShape, Color, PrimitiveType, Vertex};
+use sfml::window::{ContextSettings, VideoMode, event, window_style};
+use sfml::graphics::{RenderWindow, RenderTarget, CircleShape, Color, Vertex, Shape, Transformable, Lines, LinesStrip, VertexArray};
 
 use box2d::world::World;
 use box2d::body::BodyDef;
@@ -22,7 +22,7 @@ fn main() {
     // Create the window of the application
     let mut window = RenderWindow::new(VideoMode::new_init(800, 600, 32),
                                        "Bouncing Circle",
-                                       Close,
+                                       window_style::CLOSE,
                                        &ContextSettings::default())
                          .expect("Cannot create a new Render Window.");
 
@@ -58,36 +58,37 @@ fn main() {
                     let position = world.bodies[i].position + center;
                     circle.set_radius(radius-1.0);
                     circle.set_outline_thickness(1.0);
-                    circle.set_outline_color(&Color{red: 255, green: 0, blue: 0, alpha: 255});
+                    circle.set_outline_color(&Color::new_rgb(255, 0, 0));
                     circle.set_fill_color(&Color::transparent());
                     circle.set_position(&Vector2f::new(position.x, position.y));
                     circle.set_origin(&Vector2f::new(radius, radius));
                     window.draw(&circle);
                 },
                 box2d::shape::shape::Shape::LineShape{point1, point2} => {
+                    let mut points = VertexArray::new_init(Lines, 2).unwrap();
                     let point1_global = world.bodies[i].position + point1;
                     let point2_global = world.bodies[i].position + point2;
-                    let points = [Vertex::new_with_pos_color(&Vector2f::new(point1_global.x, point1_global.y), &Color::blue()),
-                    Vertex::new_with_pos_color(&Vector2f::new(point2_global.x, point2_global.y), &Color::blue())];
-                    window.draw_primitives(&points, PrimitiveType::Lines);
+                    points.append(&Vertex::new_with_pos_color(&Vector2f::new(point1_global.x, point1_global.y), &Color::blue()));
+                    points.append(&Vertex::new_with_pos_color(&Vector2f::new(point2_global.x, point2_global.y), &Color::blue()));
+                    window.draw(&points);
                 },
                 box2d::shape::shape::Shape::ChainLineShape{ref points} => {
-                    let mut global_points: Vec<Vertex> = Vec::new();
+                    let mut global_points = VertexArray::new_init(LinesStrip, points.len() as u32).unwrap();
                     for p in points.iter() {
                         let global_point = world.bodies[i].position + *p;
-                        global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::blue()));
+                        global_points.append(&Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::blue()));
                     }
-                    window.draw_primitives(&global_points, PrimitiveType::LinesStrip);
+                    window.draw(&global_points);
                 },
                 box2d::shape::shape::Shape::PolygonShape{ref points} => {
-                    let mut global_points: Vec<Vertex> = Vec::new();
+                    let mut global_points = VertexArray::new_init(LinesStrip, (points.len()+1) as u32).unwrap();
                     for p in points.iter() {
                         let global_point = world.bodies[i].position + *p;
-                        global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
+                        global_points.append(&Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
                     }
                     let global_point = world.bodies[i].position + points[0];
-                    global_points.push(Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
-                    window.draw_primitives(&global_points, PrimitiveType::LinesStrip);
+                    global_points.append(&Vertex::new_with_pos_color(&Vector2f::new(global_point.x, global_point.y), &Color::red()));
+                    window.draw(&global_points);
                 }
             }
         }
