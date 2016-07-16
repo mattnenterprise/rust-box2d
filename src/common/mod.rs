471048aa4b19542e0b2568833d93936697944d50
Matt McCoy <mattnenterprise@yahoo.com>
2016-07-16 11:41:58 -0400

    Adding trasnform

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -4,8 +4,10 @@ mod vec2;
 mod vec3;
 mod mat22;
 mod rot;
+mod transform;
 
 pub use self::vec2::Vec2;
 pub use self::vec3::Vec3;
 pub use self::mat22::Mat22;
 pub use self::rot::Rot;
+pub use self::transform::Transform;
---
+++ src/common/transform.rs
@@ -0,0 +1,30 @@
+use super::Vec2;
+use super::Rot;
+
+/// A transform contains translation and rotation. It is used to represent
+/// the position and orientation of rigid frames.
+pub struct Transform {
+    pub p: Vec2,
+    pub q: Rot
+}
+
+impl Transform {
+    pub fn new(position: Vec2, rotation: Rot) -> Transform {
+        return Transform {
+            p: position,
+            q: rotation
+        }
+    }
+
+    /// Set this to the identity transform.
+    pub fn set_identity(&mut self) {
+        self.p.set_zero();
+        self.q.set_identity();
+    }
+
+    /// Set this based on the position and angle.
+    pub fn set(&mut self, position: Vec2, angle: f32) {
+        self.p = position;
+        self.q.set(angle);
+    }
+}
