77e4e42ddba538049c84eda0b4b0af37774dd882
Matt McCoy <mattnenterprise@yahoo.com>
2016-07-19 20:25:50 -0400

    Adding simplex

--- src/collision/simplex.rs
+++ src/collision/simplex.rs
@@ -1,3 +1,38 @@
 pub struct Simplex {
-    
+    pub m_v1: SimplexVertex,
+    pub m_v2: SimplexVertex,
+    pub m_v3: SimplexVertex,
+    pub m_count: i32
+}
+
+impl Simplex {
+    // TODO implement
+    // pub fn read_cache()
+
+    // TODO implement
+    // pub fn write_cache()
+
+    // TODO implement {
+    // pub fn get_search_direction() -> Vec2
+
+    // TODO implement
+    // pub fn get_closest_point() -> Vec2
+
+    // TODO implement
+    // pub fn get_witness_points()
+
+    // TODO implement
+    pub fn get_metric() -> f32 {
+        0.0
+    }
+
+    // TODO implement
+    pub fn solve_2() {
+
+    }
+
+    // TODO implement
+    pub fn solve_3() {
+
+    }
 }
