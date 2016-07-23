a62827a14d8c13e3a9d9e1815a6b9bfa4f785a49
Matt McCoy <mattnenterprise@yahoo.com>
2016-07-23 11:24:26 -0400

    Adding get_witness_points to Simplex

--- src/collision/simplex.rs
+++ src/collision/simplex.rs
@@ -22,8 +22,29 @@ impl Simplex {
     // TODO implement
     // pub fn get_closest_point() -> Vec2
 
-    // TODO implement
-    // pub fn get_witness_points()
+    pub fn get_witness_points(&self) -> (Vec2, Vec2) {
+        match self.m_count {
+            0 => {
+                assert!(false);
+                return (Vec2::zero(), Vec2::zero());
+            },
+            1 => {
+                return (self.m_v1.w_a, self.m_v1.w_b);
+            },
+            2 => {
+                return (self.m_v1.a * self.m_v1.w_a + self.m_v2.a * self.m_v2.w_a,
+                    self.m_v1.a * self.m_v1.w_b + self.m_v2.a * self.m_v2.w_b)
+            },
+            3 => {
+                let p_a = self.m_v1.a * self.m_v1.w_a + self.m_v2.a * self.m_v2.w_a + self.m_v3.a * self.m_v3.w_a;
+                return (p_a, p_a)
+            },
+            _ => {
+                assert!(false);
+                return (Vec2::zero(), Vec2::zero());
+            }
+        }
+    }
 
     pub fn get_metric(&self) -> f32 {
         match self.m_count {
